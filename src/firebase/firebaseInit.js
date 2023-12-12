9a412e896d99432bf6e1c34cc99bf12734c43da6
Gerald Ingraham <geraldingraham@Geralds-MBP.attlocal.net>
2023-12-11 19:55:47 -0500

    first commit of vue blog. firebase backend connected some styling added

---
+++ src/firebase/firebaseInit.js
@@ -0,0 +1,19 @@
+// Frontend hook up to the firebase backend
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCVMSCavkxQLJfluRHVELZtdak0wbuqebQ",
+    authDomain: "vuefirebaseblog-cf9af.firebaseapp.com",
+    projectId: "vuefirebaseblog-cf9af",
+    storageBucket: "vuefirebaseblog-cf9af.appspot.com",
+    messagingSenderId: "362538943156",
+    appId: "1:362538943156:web:5d48d137f0f98b52f8cde1"
+  };
+
+  // Initialize Firebase
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const timestamp = firebase.firestore.FieldValue.serverTimestamp;
+
+export {timestamp}
+export default firebaseApp.firestore()
---
+++ src/views/Home.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="home">
+    <BlogPost :post="welcomeScreen"/>
+    <BlogPost v-for="(post,index) in sampleBlogPost" :key="index" :post="post"/>
+  </div>
+</template>
+
+<script>
+import BlogPost from '../components/BlogPost.vue'
+export default {
+  name: "Home",
+  components: {BlogPost},
+  data() {
+    return {
+      welcomeScreen: {
+        title: "Welcome",
+        blogPost:
+          "Weekly blog articles wit all things programming including HTML, CSS, Javascript and more.",
+        welcomeScreen: true,
+        photo: "coding",
+      },
+      sampleBlogPost: [
+        {
+          title: "this is a filler title!",
+          blogHTML: "This is a filler blog post title!",
+          blogCoverPhoto: "beautiful-stories",
+        }, 
+        {
+          title: "this is a filler title!",
+          blogHTML: "This is a filler blog post title!",
+          blogCoverPhoto: "designed-for-everyone",
+        }
+      ]
+    }
+  }
+};
+</script>
